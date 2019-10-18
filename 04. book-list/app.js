e4a82c3834cacb228bda876ed78bf2661a662a5d
OVER-Studio <jem_moussa@live.fr>
2019-10-18 15:33:29 +0000

    Phone Book - add structure

---
+++ 04. book-list/app.js
@@ -0,0 +1,14 @@
+// Model Controler
+const contactModel = (function() {
+
+})();
+
+// UI Controler
+const uiControler = (function() {
+
+})();
+
+// Global Controler
+const mainControler = (function() {
+
+})();
